90f03422945d4f9bb6ba35f1e19f485385c3dbe7
Juhaszky <juhasz.krisztian2000@gmail.com>
2024-10-30 17:03:55 +0100

    feat(postEventListener): postEvnetListener service added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { PostService } from './post.service';
 import { UserService } from './user.service';
 import { PrismaService } from './prisma.service';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { PostEventListenerService } from './post-event-listener/post-event-listener.service';
 
 @Module({
-  imports: [],
+  imports: [EventEmitterModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService, PostService, UserService, PrismaService],
+  providers: [AppService, PostService, UserService, PrismaService, PostEventListenerService],
 })
 export class AppModule {}
