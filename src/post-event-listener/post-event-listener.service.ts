ffea4c8ed059c2f4ed6a703907457c00233a37ce
Juhaszky <juhasz.krisztian2000@gmail.com>
2024-10-31 14:45:50 +0100

    feat(post-event-listener-service): basic log added on posts fetch

--- src/post-event-listener/post-event-listener.service.ts
+++ src/post-event-listener/post-event-listener.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
+import { Post } from '@prisma/client';
 
 @Injectable()
 export class PostEventListenerService {
-  @OnEvent('posts.get')
-  handlePostGetEvent(payload: { postName: string }) {
-    console.log(`Post Get Event Triggered with postname ${payload.postName}`);
+  @OnEvent('posts.fetched')
+  handlePostGetEvent(posts: Post[]) {
+    console.log(`Post Get Event Triggered with postname`);
+    posts.forEach((post) => console.log(post));
   }
 }
