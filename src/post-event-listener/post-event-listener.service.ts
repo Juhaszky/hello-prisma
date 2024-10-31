d1965c276091d8b64ea312faf98c74f3764b6d84
Juhaszky <juhasz.krisztian2000@gmail.com>
2024-10-31 16:21:02 +0100

    feat(post-event-listener): handlePostCreatedEvent logic implementation  ➕

--- src/post-event-listener/post-event-listener.service.ts
+++ src/post-event-listener/post-event-listener.service.ts
@@ -1,12 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { Post } from '@prisma/client';
+import { PrismaReadService } from 'src/prisma/prisma-read.service';
+import { PrismaWriteService } from 'src/prisma/prisma-write.service';
 
 @Injectable()
 export class PostEventListenerService {
+  constructor(
+    private readonly prismaWrite: PrismaWriteService,
+    private readonly prismaRead: PrismaReadService,
+  ) {}
   @OnEvent('posts.fetched')
   handlePostGetEvent(posts: Post[]) {
     console.log(`Post Get Event Triggered with postname`);
     posts.forEach((post) => console.log(post));
+  }
+  @OnEvent('posts.created')
+  async handlePostCreatedEvent(payload: { postId: number }) {
+    const post = await this.prismaWrite.post.findUnique({
+      where: { id: payload.postId },
+    });
+    if (post) {
+      const author = await this.prismaWrite.user.findUnique({
+        where: { id: post.authorId },
+      });
+      await this.prismaRead.post.create({
+        data: {
+          title: post.title,
+          content: post.content,
+          author: { connect: { email: author.email } },
+        },
+      });
+    }
   }
 }
